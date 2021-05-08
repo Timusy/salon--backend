ad0f410c3676e9fa2523455e782354177a4fb8ea
Timusy <sumitchoudhary2686@gmail.com>
2021-05-08 09:37:12 +0530

    User/Admin sign-up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const env = require("dotenv");
-
-const app = express();
-app.use(express.json());
+const bodyParser=require("body-parser");
+const mongoose = require('mongoose');
+const userRoutes=require("./routes/user");
 
 env.config();
-app.get("/",(req,res)=>{
-    res.send("Hello");
-})
+
+const app = express();
+//app.use(express.json());
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.fobqx.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,{ useNewUrlParser: true , useUnifiedTopology: true,useCreateIndex:true}).then(console.log("Database connected"));
+
+
+app.use('/api',userRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on ${process.env.PORT}`);
---
+++ src/models/user.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const bcrypt=require("bcrypt");
+
+var validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name not Specifeid"],
+        trim: true
+    },
+
+    lastName: {
+        type: String,
+        trim: true,
+        default:""
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: [true, 'Email address is required'],
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+
+    hash_password: {
+        type: String,
+        required: [true, "Password is required"],
+
+    },
+
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+
+    contactNumber: {
+        type: String,
+        min: 10,
+        max: 10
+    },
+
+    profilePicture: {
+        type: String
+    }
+
+}, { timestamps: true });
+
+userSchema.virtual('password')
+.set(function(password){
+  this.hash_password=bcrypt.hashSync(password,10);
+});
+
+userSchema.methods = {
+    authenticate: function(password) {
+      return  bcrypt.compareSync(password,this.hash_password);
+  
+    }
+  };
+
+module.exports = mongoose.model("User",userSchema);
